00a3b110b3162bc1702eec3b7109cba9413d8cfe
Andrii Babanin <andrii.babanin@gmail.com>
2021-07-29 17:16:17 +0300

    Create Sample project

---
+++ Sample/Lemma.AADS.CollectionsSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lemma.AADS.CollectionsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new int[] { 1, 2, 3, 4, 5 };
+            var list = new Structures.MyVector<int>();
+
+            foreach (var i in items)
+                list.Add(i);
+
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);   
+            }
+            
+        }
+    }
+}
